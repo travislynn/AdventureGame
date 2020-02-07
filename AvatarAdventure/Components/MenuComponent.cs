01d94746bb3e67b2bd78e4cede44b81236e34ba5
Travis Lynn <travis.lynn@veteransunited.com>
2020-02-07 15:54:13 -0600

    New assets, main menu

--- AvatarAdventure/Components/MenuComponent.cs
+++ AvatarAdventure/Components/MenuComponent.cs
@@ -12,6 +12,7 @@ namespace AvatarAdventure.Components
     public class MenuComponent
     {
         #region Fields
+
         SpriteFont spriteFont;
         readonly List<string> menuItems = new List<string>();
         int selectedIndex = -1;
@@ -26,6 +27,7 @@ namespace AvatarAdventure.Components
         #endregion Fields
 
         #region Properties
+
         public Vector2 Postion
         {
             get { return position; }
@@ -66,12 +68,14 @@ namespace AvatarAdventure.Components
         }
         #endregion Properties
         #region Constructors
+
         public MenuComponent(SpriteFont spriteFont, Texture2D texture)
         {
             this.mouseOver = false;
             this.spriteFont = spriteFont;
             this.texture = texture;
         }
+
         public MenuComponent(SpriteFont spriteFont, Texture2D texture, string[] menuItems)
         : this(spriteFont, texture)
         {
@@ -82,8 +86,10 @@ namespace AvatarAdventure.Components
             }
             MeassureMenu();
         }
+
         #endregion Constructors
         #region Methods
+
         public void SetMenuItems(string[] items)
         {
             menuItems.Clear();
@@ -112,8 +118,7 @@ namespace AvatarAdventure.Components
             mouseOver = false;
             for (int i = 0; i < menuItems.Count; i++)
             {
-                buttonRect = new Rectangle((int)menuPosition.X, (int)menuPosition.Y,
-                texture.Width, texture.Height);
+                buttonRect = new Rectangle((int)menuPosition.X, (int)menuPosition.Y, texture.Width, texture.Height);
                 if (buttonRect.Contains(p))
                 {
                     selectedIndex = i;
@@ -144,13 +149,11 @@ namespace AvatarAdventure.Components
                     myColor = HiliteColor;
                 else
                     myColor = NormalColor;
+
                 spriteBatch.Draw(texture, menuPosition, Color.White);
                 Vector2 textSize = spriteFont.MeasureString(menuItems[i]);
                 Vector2 textPosition = menuPosition + new Vector2((int)(texture.Width - textSize.X) / 2, (int)(texture.Height - textSize.Y) / 2);
-                spriteBatch.DrawString(spriteFont,
-                menuItems[i],
-                textPosition,
-                myColor);
+                spriteBatch.DrawString(spriteFont, menuItems[i], textPosition, myColor);
                 menuPosition.Y += texture.Height + 50;
             }
         }
@@ -158,5 +161,6 @@ namespace AvatarAdventure.Components
         #endregion Methods
         #region Virtual Methods
         #endregion Virtual Methods
-    }
+    }
+
 }
---
+++ AvatarAdventure/GameStates/MainMenuState.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AvatarAdventure.Components;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+
+namespace AvatarAdventure.GameStates
+{
+    public class MainMenuState : BaseGameState, IMainMenuState
+    {
+        #region Field Region
+
+        Texture2D background;
+        SpriteFont spriteFont;
+        MenuComponent menuComponent;
+
+        #endregion
+        #region Property Region
+        #endregion
+        #region Constructor Region
+
+        public MainMenuState(Game game)
+            : base(game)
+        {
+            game.Services.AddService(typeof(IMainMenuState), this);
+        }
+
+        #endregion
+        #region Method Region
+
+        public override void Initialize()
+        {
+            base.Initialize();
+        }
+        protected override void LoadContent()
+        {
+            spriteFont = Game.Content.Load<SpriteFont>(@"Fonts\InterfaceFont");
+            background = Game.Content.Load<Texture2D>(@"GameScreens\menuscreen");
+            Texture2D texture = Game.Content.Load<Texture2D>(@"Misc\wooden-button");
+            string[] menuItems = { "NEW GAME", "CONTINUE", "OPTIONS", "EXIT" };
+            menuComponent = new MenuComponent(spriteFont, texture, menuItems);
+            Vector2 position = new Vector2();
+            position.Y = 90;
+            position.X = 1200 - menuComponent.Width;
+            menuComponent.Postion = position;
+            base.LoadContent();
+        }
+        public override void Update(GameTime gameTime)
+        {
+            menuComponent.Update(gameTime);
+            if (Xin.CheckKeyReleased(Keys.Space) || Xin.CheckKeyReleased(Keys.Enter) ||
+                (menuComponent.MouseOver && Xin.CheckMouseReleased(MouseButtons.Left)))
+            {
+                if (menuComponent.SelectedIndex == 0)
+                {
+                    Xin.FlushInput();
+                }
+                else if (menuComponent.SelectedIndex == 1)
+                {
+                    Xin.FlushInput();
+                }
+                else if (menuComponent.SelectedIndex == 2)
+                {
+                    Xin.FlushInput();
+                }
+                else if (menuComponent.SelectedIndex == 3)
+                {
+                    Game.Exit();
+                }
+            }
+            base.Update(gameTime);
+        }
+        public override void Draw(GameTime gameTime)
+        {
+            GameRef.SpriteBatch.Begin();
+            GameRef.SpriteBatch.Draw(background, Vector2.Zero, Color.White);
+            GameRef.SpriteBatch.End();
+            base.Draw(gameTime);
+            GameRef.SpriteBatch.Begin();
+            menuComponent.Draw(gameTime, GameRef.SpriteBatch);
+            GameRef.SpriteBatch.End();
+        }
+        #endregion
+    }
+}
