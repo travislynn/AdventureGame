20a966ee2d0d9a78137a39a93e09a2489330cae7
Travis Lynn <travis.lynn@veteransunited.com>
2020-02-08 00:59:25 -0600

    character set up

---
+++ AvatarAdventure/CharacterComponents/ICharacter.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AvatarAdventure.AvatarComponents;
+using AvatarAdventure.TileEngine;
+
+namespace AvatarAdventure.CharacterComponents
+{
+    public interface ICharacter
+    {
+        string Name { get; }
+        AnimatedSprite Sprite { get; }
+        Avatar BattleAvatar { get; }
+        Avatar GiveAvatar { get; }
+        void SetConversation(string newConversation);
+        void Update(GameTime gameTime);
+        void Draw(GameTime gameTime, SpriteBatch spriteBatch);
+    }
+}
